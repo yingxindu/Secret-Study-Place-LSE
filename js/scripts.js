38a968d3e483825eb513b14dc9e85ed8427f2d8d
Yingxin <y.du15@lse.ac.uk>
2018-11-30 10:10:13 +0000

    renewed navbar

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("mission");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
